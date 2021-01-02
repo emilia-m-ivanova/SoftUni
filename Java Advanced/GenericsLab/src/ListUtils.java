9228c888304b3fdc831c55ec638fc3fe9caf0c2d
emilia-m-ivanova <emilia.m.ivanova@gmail.com>
2021-01-02 20:05:47 +0200

    Add exercises

--- Java Advanced/GenericsLab/src/ListUtils.java
+++ Java Advanced/GenericsLab/src/ListUtils.java
@@ -3,21 +3,15 @@ import java.util.List;
 
 public class ListUtils {
 
-    public static <T extends Comparable<T>> T getMin(List<T> list) throws IllegalArgumentException {
-        if(list.isEmpty()){
-            throw new IllegalArgumentException();
-        }
+    public static <T extends Comparable<T>> T getMin(List<T> list) {
         return list.stream()
                 .min(Comparator.naturalOrder())
-                .get();
+                .orElseThrow(IllegalArgumentException::new);
     }
 
-    public static <T extends Comparable<T>> T getMax(List<T> list) throws IllegalArgumentException {
-        if(list.isEmpty()){
-            throw new IllegalArgumentException();
-        }
+    public static <T extends Comparable<T>> T getMax(List<T> list) {
         return list.stream()
                 .max(Comparator.naturalOrder())
-                .get();
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
