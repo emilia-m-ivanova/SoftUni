4f095788d53594fbeb6c5b40626a13ae46a3f7ad
emilia-m-ivanova <emilia.m.ivanova@gmail.com>
2021-01-26 17:15:42 +0200

    Add exercises

--- JS Advanced/DOMIntroductionExercise/08. Generate Report/report.js
+++ JS Advanced/DOMIntroductionExercise/08. Generate Report/report.js
@@ -1,3 +1,18 @@
-function generateReport(colNames) {
-    //TODO
+function generateReport() {
+    const checkBoxes = Array.from(document.querySelectorAll('thead th input'));
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+    const properties = [];
+    checkBoxes.forEach((box,index)=>{
+        if(box.checked){
+            properties.push([box.name, index]);
+        }
+    });
+    rows = rows.map(row=>{
+        const objectRow = {};
+        properties.forEach(property=>{
+            objectRow[property[0]] = row.children[property[1]].textContent;
+        })
+        return objectRow;
+    })
+    document.getElementById('output').textContent = JSON.stringify(rows);
 }
--- JS Advanced/DOMIntroductionExercise/09. Number Convertor/solution.js
+++ JS Advanced/DOMIntroductionExercise/09. Number Convertor/solution.js
@@ -1,4 +1,23 @@
 function solve() {
+    const selectMenu = document.querySelector('#selectMenuTo');
+    const hexa = document.createElement('option');
+    hexa.setAttribute('value','hexadecimal');
+    hexa.textContent = 'Hexadecimal';
+    selectMenu.appendChild(hexa);
+    const binary = document.createElement('option');
+    binary.setAttribute('value','binary');
+    binary.textContent = 'Binary';
+    selectMenu.appendChild(binary);
 
-    //TODO...
+    function onClick() {
+        const options = Array.from(selectMenu.children);
+        let number = Number(document.querySelector('#input').value);
+        if(options[1].selected){
+            number = number.toString(16).toUpperCase();
+        }else if(options[2].selected){
+            number = number.toString(2);
+        }
+        document.querySelector('#result').value = number;
+    }
+    document.querySelector('button').addEventListener("click", onClick);
 }
