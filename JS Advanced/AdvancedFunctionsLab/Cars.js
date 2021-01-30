491dbfd936f2531d24461e7c4f33b8282ee388e6
emilia-m-ivanova <emilia.m.ivanova@gmail.com>
2021-01-30 22:11:30 +0200

    Add exercises

--- JS Advanced/AdvancedFunctionsLab/Cars.js
+++ JS Advanced/AdvancedFunctionsLab/Cars.js
@@ -2,8 +2,8 @@ function solve(arr) {
     function generateData() {
         const data = {};
         return {
-            create: (name, inherit, parentName) => {
-                data[name] = inherit ? Object.create(data[parentName])
+            create: (name, _, parentName) => {
+                data[name] = parentName ? Object.create(data[parentName])
                     : {};
             },
             set: (name, key, value) => {
@@ -25,9 +25,9 @@ function solve(arr) {
     })
 }
 
-solve(['create pesho',
-    'create gosho inherit pesho',
-    'create stamat inherit gosho',
-    'set pesho rank number1',
-    'set gosho nick goshko',
-    'print stamat']);
+solve(['create c1',
+    'create c2 inherit c1',
+    'set c1 color red',
+    'set c2 model new',
+    'print c1',
+    'print c2']);
