10a99c27c4b31183dd25f257e9b6f82c9c7b5f92
emilia-m-ivanova <emilia.m.ivanova@gmail.com>
2021-02-07 20:33:55 +0200

    Add exercises

---
+++ JS Advanced/ClassesAndAttributesLab/04. Point Distance.js
@@ -0,0 +1,10 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(point1, point2) {
+        return Math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2);
+    }
+}
