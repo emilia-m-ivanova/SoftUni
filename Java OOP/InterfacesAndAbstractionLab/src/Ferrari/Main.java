486b3323a2b729faa79711136df07d0cba281aa4
emilia-m-ivanova <emilia.m.ivanova@gmail.com>
2021-02-18 09:18:24 +0200

    Add exercises

---
+++ Java OOP/InterfacesAndAbstractionLab/src/Ferrari/Main.java
@@ -0,0 +1,8 @@
+package Ferrari;
+
+public class Main {
+    public static void main(String[] args) {
+        Ferrari ferrari = new Ferrari("Dominic Toretto");
+        System.out.println(ferrari.toString());
+    }
+}
