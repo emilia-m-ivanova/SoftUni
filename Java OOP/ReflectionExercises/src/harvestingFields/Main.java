6786d92ab91080f7fddd39920f7e5bc65a43e1bc
emilia-m-ivanova <emilia.m.ivanova@gmail.com>
2021-03-11 10:36:57 +0200

    Add exercises

--- Java OOP/ReflectionExercises/src/harvestingFields/Main.java
+++ Java OOP/ReflectionExercises/src/harvestingFields/Main.java
@@ -1,15 +1,19 @@
 package harvestingFields;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws ClassNotFoundException, IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         Class<RichSoilLand> clazz = RichSoilLand.class;
         Field[] fields = clazz.getDeclaredFields();
-        String input = scanner.nextLine();
+        String input = bf.readLine();
         while (!input.equals("HARVEST")) {
             for (Field field : fields) {
                 if (Modifier.toString(field.getModifiers()).equals(input) || input.equals("all")) {
@@ -17,7 +21,7 @@ public class Main {
                             field.getType().getSimpleName() + " " + field.getName());
                 }
             }
-            input = scanner.nextLine();
+            input = bf.readLine();
         }
     }
 }
