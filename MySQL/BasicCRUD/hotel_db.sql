692dc80ed430a28545fca550bdd1078d48bd29fc
emilia-m-ivanova <emilia.m.ivanova@gmail.com>
2021-05-19 10:25:56 +0300

    Add exercises

--- MySQL/BasicCRUD/hotel_db.sql
+++ MySQL/BasicCRUD/hotel_db.sql
@@ -51,3 +51,34 @@ CREATE TABLE clients (
 INSERT INTO clients(`first_name`,`last_name`,`room_id`) 
 VALUES('Pesho','Petrov', 1),('Gosho','Georgiev', 2),
 ('Mariya','Marieva', 2), ('Katya','Katerinova', 1), ('Nikolay','Nikolaev', 2);
+
+SELECT id, first_name, last_name, job_title 
+FROM `employees` 
+ORDER BY `id`;
+
+SELECT id, concat(first_name, ' ', last_name) AS full_name, job_title, salary
+FROM `employees` 
+WHERE `salary` > 1000
+ORDER BY `id`;
+
+UPDATE `employees`
+SET `salary` = `salary` + 100
+WHERE `job_title` = 'Manager';
+SELECT `salary`
+FROM `employees`;
+
+CREATE VIEW `top_paid` AS
+SELECT * FROM `employees`
+ORDER BY `salary` DESC
+LIMIT 1;
+SELECT * FROM `top_paid`;
+
+SELECT * FROM `employees`
+WHERE `department_id` = 4 
+AND `salary` >= 1000
+ORDER BY `id`;
+
+DELETE FROM `employees`
+WHERE `department_id` BETWEEN 1 AND 2;
+SELECT * FROM `employees`
+ORDER BY `id`;
