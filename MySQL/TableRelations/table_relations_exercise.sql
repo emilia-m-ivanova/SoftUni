5445b086366f88c0521baef5c2ef4f83ae1aa222
emilia-m-ivanova <emilia.m.ivanova@gmail.com>
2021-05-30 14:54:35 +0300

    Add exercises

--- MySQL/TableRelations/table_relations_exercise.sql
+++ MySQL/TableRelations/table_relations_exercise.sql
@@ -117,3 +117,12 @@ INSERT INTO `teachers` VALUES
 (102, 'Maya', 106),
 (103, 'Silvia', 106);
 
+-- 9
+USE `geography`;
+
+SELECT m.`mountain_range`, p.`peak_name`, p.`elevation` AS `peak_elevation`
+FROM `mountains` AS m
+JOIN `peaks` AS p
+ON m.`id` = p.`mountain_id`
+WHERE `mountain_range` = 'Rila'
+ORDER BY `peak_elevation` DESC;
